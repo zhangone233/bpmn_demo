56cc8f037556e291e657e268f02cef34fbdeb6ca
张一 <zhangyi.byte@bytedance.com>
2022-08-24 02:41:49 +0800

    feat: 有bug版本

--- src/pages/custom/auto-place/BpmnAutoPlaceUtil.js
+++ src/pages/custom/auto-place/BpmnAutoPlaceUtil.js
@@ -23,7 +23,7 @@ import {
  *
  * @return {Point}
  */
-export function getNewShapePosition(source, element, elementRegistry) {
+export function getNewShapePosition(source, element, elementRegistry, modeling) {
   console.log(element,'element');
 
   if (is(element, 'bpmn:TextAnnotation')) {
@@ -36,14 +36,75 @@ export function getNewShapePosition(source, element, elementRegistry) {
   }
 
   if (is(element, 'bpmn:FlowNode')) {
-    console.log('2222');
 
-    return getFlowNodePosition(source, element, elementRegistry);
+    return getFlowNodePosition(source, element, elementRegistry, modeling);
   }
 }
 
+// 往下挪动
+const outerShapeMove = (source, shapeLowest, outerShapeMoveY, allShape, modeling) => {
+  // 获取最低位图形所有的连接线节点
+  const incoming = shapeLowest?.incoming;
+  if(!incoming?.length){
+    return;
+  }
+
+  // 判断新建图形的父级点是否也是最地位图形的父级点（准确来说是连接点，目前并没有限制一个元素只能连接一个）。
+  // 如果也是父级连接点，那么就不需要往下挪动了。
+  const isUndesiredMove = incoming.some(connection => connection.source === source);
+  if(isUndesiredMove){
+    return;
+  }
+
+  // 如果最低位图形的位置在source父级图形之上，那么也无需下移
+  if((shapeLowest.y + shapeLowest.height) < source.y){
+    return;
+  }
+
+  // 父节点右上角坐标点
+  const parentRightX = source.x + source.width;
+  const parentRightY = source.y;
+
+  // 以父节点右上角为点，90°直角系为范畴。 
+  // 凡是其它元素的四角坐标其中一个轴有小于这个90°直角坐标点的话，
+  // 都视为需要下移的对象
+
+  const needMoveShapeList = allShape.filter(shape => {
+    const { x: shapeX, y: shapeY, width, height } = shape;
+
+    const leftUp = {
+      x: shapeX,
+      y: shapeY,
+    };
+    const leftDown = {
+      x: shapeX,
+      y: shapeY + height,
+    };
+    const rightUp = {
+      x: shapeX + width,
+      y: shapeY,
+    };
+    const rightDown = {
+      x: shapeX + width,
+      y: shapeY + height,
+    };
+
+    // 四个角坐标有一个处于直角系范围之内的，都往下调整位置
+    return [leftUp,leftDown,rightUp,rightDown].some(({ x, y }) => {
+      return (x <= parentRightX) && (y >= parentRightY);
+    });
+  });
+  console.log(needMoveShapeList,'needMoveShapeList');
+
+  modeling.moveElements(needMoveShapeList, {
+    x: 0,
+    y: outerShapeMoveY,
+  });
+
+}
+
 // 自定义的方法
-export function customGetCoordinate (lastPosition, elementRegistry, source, element) {
+export function customGetCoordinate (lastPosition, elementRegistry, modeling, source, element) {
   const allShape = elementRegistry.getAll()[0].children || []; // 获取流程所有图形shape对象
   // console.log(allShape, 'shape');
 
@@ -58,71 +119,156 @@ export function customGetCoordinate (lastPosition, elementRegistry, source, elem
   const bottomY = offsetY + element.height;
 
   const margin = 30; // 固定的图形上下边距长度 （插件默认也是30）
-  let moveY = 0; // 记录Y轴下次递归应该增加的距离
+  // let moveY = 0; // 记录Y轴下次递归应该增加的距离
 
-  // 用来判断
-  const isExistPoint = ({x, y}) => {
-   return (x >= offsetX && x <= rightX) && (y >= offsetY && y <= bottomY)
-  }
+  const isisExistPointX = (x) => (x >= offsetX && x <= rightX);
+  const isisExistPointY = (y) => (y >= offsetY && y <= bottomY);
 
-  // 递归标识
-  const isExistCoverage = allShape.some(shape => {
+  // // 用来判断
+  // const isExistPoint = ({x, y}) => {
+  //   return isisExistPointX(x) && isisExistPointY(y);
+  //  };
+  
+  // // 递归标识
+  // const isExistCoverage = allShape.some(shape => {
    
-    // 只判断task元素
-    if(is(shape, "bpmn:Task")) {
-      const { x: shapeX, y: shapeY, width, height } = shape;
+  //   // 只判断task元素
+  //   if(is(shape, "bpmn:Task")) {
+  //     const { x: shapeX, y: shapeY, width, height } = shape;
 
-      const leftUp = {
-        x: shapeX,
-        y: shapeY,
-      }
-      const leftDown = {
-        x: shapeX,
-        y: shapeY + height,
-      }
-      const rightUp = {
-        x: shapeX + width,
-        y: shapeY,
-      }
-      const rightDown = {
-        x: shapeX + width,
-        y: shapeY + height,
-      }
+  //     const leftUp = {
+  //       x: shapeX,
+  //       y: shapeY,
+  //     }
+  //     const leftDown = {
+  //       x: shapeX,
+  //       y: shapeY + height,
+  //     }
+  //     const rightUp = {
+  //       x: shapeX + width,
+  //       y: shapeY,
+  //     }
+  //     const rightDown = {
+  //       x: shapeX + width,
+  //       y: shapeY + height,
+  //     }
 
-      // 四个角坐标有一个处于新建图形大小范围之内的，都往下调整位置
-      const isBreak = [leftUp,leftDown,rightUp,rightDown].some(point => isExistPoint(point))
+  //     // 四个角坐标有一个处于新建图形大小范围之内的，都往下调整位置
+  //     const isBreak = [leftUp,leftDown,rightUp,rightDown].some(point => isExistPoint(point))
 
-      if(isBreak){
-        // 正在占位图形的下Y轴坐标 + 间距
-        moveY = shapeY + height + margin;
-      }
+  //     // if(isBreak){
+  //     //   // 正在占位图形的下Y轴坐标 + 间距
+  //     //   moveY = shapeY + height + margin;
+  //     // }
 
-      return isBreak;
+  //     return isBreak;
       
-      // return (shapeX >= offsetX && shapeX <= rightX) && (shapeY >= offsetY && shapeY <= bottomY)
-    }
+  //     // return (shapeX >= offsetX && shapeX <= rightX) && (shapeY >= offsetY && shapeY <= bottomY)
+  //   }
+  // });
+  // // 如果可以直接
+  // if(!isExistCoverage) {
+  //   return lastPosition;
+  // }
+
+  // if(offsetY === source.y && !isExistCoverage){
+  //   return lastPosition;
+  // }
+
+  // 检索出与新建元素所处于同一列的所有元素
+  const inColumnsShape = allShape.filter(shape => {
+    const { x: shapeX, y: shapeY, width, height } = shape;
+    const rightX = shapeX + width;
+
+    return [shapeX, rightX].some(isisExistPointX);
   });
 
-  console.log(isExistCoverage, 'isExistCoverage');
+  // 找出同一列下，最低位的那个元素 (以下X坐标为准)
+  const shapeLowest = inColumnsShape.sort((a,b) => (b.y + b.height) - (a.y + a.height))?.[0];
 
-  if(isExistCoverage) {
-    // lastPosition.x = lastPosition.x + element.width / 2; // 如果需要平移，可以修改x轴位置
-    // lastPosition.y = lastPosition.y + element.height / 2; // 往下移动半个身位
+  console.log(shapeLowest, 'shapeLowest');
+  console.log(inColumnsShape, 'inColumnsShape');
+
+  if(shapeLowest && is(shapeLowest, "bpmn:Task")){
+    const moveY = shapeLowest.y + shapeLowest.height + margin;
     lastPosition.y = moveY + element.height / 2; // 得到中心点的坐标
 
-    // 递归继续判断位置，直至找到一块空地
-    return customGetCoordinate(lastPosition, elementRegistry, source, element)
+    const outerShapeMoveY = (lastPosition.y - element.height / 2) - source.y;
+    outerShapeMove(source, shapeLowest, outerShapeMoveY, allShape, modeling);
   }
 
   // 返回最终坐标 （这个是以图形中心）
   return lastPosition;
+
+
+  // // 用来判断
+  // const isExistPoint = ({x, y}) => {
+  //   return isisExistPointX(x) && isisExistPointY(y);
+  //  };
+  
+  // // 递归标识
+  // const isExistCoverage = allShape.some(shape => {
+   
+  //   // 只判断task元素
+  //   if(is(shape, "bpmn:Task")) {
+  //     const { x: shapeX, y: shapeY, width, height } = shape;
+
+  //     const leftUp = {
+  //       x: shapeX,
+  //       y: shapeY,
+  //     }
+  //     const leftDown = {
+  //       x: shapeX,
+  //       y: shapeY + height,
+  //     }
+  //     const rightUp = {
+  //       x: shapeX + width,
+  //       y: shapeY,
+  //     }
+  //     const rightDown = {
+  //       x: shapeX + width,
+  //       y: shapeY + height,
+  //     }
+
+  //     // 四个角坐标有一个处于新建图形大小范围之内的，都往下调整位置
+  //     const isBreak = [leftUp,leftDown,rightUp,rightDown].some(point => isExistPoint(point))
+
+  //     if(isBreak){
+  //       // 正在占位图形的下Y轴坐标 + 间距
+  //       moveY = shapeY + height + margin;
+  //     }
+
+  //     return isBreak;
+      
+  //     // return (shapeX >= offsetX && shapeX <= rightX) && (shapeY >= offsetY && shapeY <= bottomY)
+  //   }
+  // });
+
+  // console.log(isExistCoverage, 'isExistCoverage');
+
+  // if(isExistCoverage) {
+  //   // lastPosition.x = lastPosition.x + element.width / 2; // 如果需要平移，可以修改x轴位置
+  //   // lastPosition.y = lastPosition.y + element.height / 2; // 往下移动半个身位
+  //   lastPosition.y = moveY + element.height / 2; // 得到中心点的坐标
+
+  //   // 递归继续判断位置，直至找到一块空地
+  //   return customGetCoordinate(lastPosition, elementRegistry, source, element)
+  // }
+
+  // if(shapeLowest && is(shapeLowest, "bpmn:Task")){
+  //   const moveY = shapeLowest.y + shapeLowest.height + margin;
+  //   lastPosition.y = moveY + element.height / 2; // 得到中心点的坐标
+  // }
+
+  // // 返回最终坐标 （这个是以图形中心）
+  // return lastPosition;
 }
 
 /**
  * Always try to place element right of source;
  * compute actual distance from previous nodes in flow.
  */
-export function getFlowNodePosition(source, element, elementRegistry) {
+export function getFlowNodePosition(source, element, elementRegistry, modeling) {
 
   var sourceTrbl = asTRBL(source);
   var sourceMid = getMid(source);
@@ -160,7 +306,7 @@ export function getFlowNodePosition(source, element, elementRegistry) {
   const lastPosition = findFreePosition(source, element, position, generateGetNextPosition(nextPositionDirection));
 
   // 自定义获取
-  return customGetCoordinate(lastPosition, elementRegistry, source, element);
+  return customGetCoordinate(lastPosition, elementRegistry, modeling, source, element);
 }
 
 
